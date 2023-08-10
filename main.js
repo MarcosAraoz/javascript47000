62d9e70f4d8ed35e82f667861bd699de40912a29
Marcos Araoz <araozmarcos11@gmail.com>
2023-08-10 19:43:14 -0300

    nueva__entrega

--- main.js
+++ main.js
@@ -1,26 +1,45 @@
-let producto = parseInt(prompt("Selecciona el producto: 1.Zapatillas - 2.Buzo - 3.Remera"))
-let totalCompra = 0 
-let decision 
+let producto = parseInt (prompt("Selecciona el producto que deseas comprar 1.Buzo - 2.Remera - 3.Zapatillas"))
+
 let seguirComprando = true
+let decision
+const carritos = []
+
+const buzos = [
+    {id:1, nombre:"Off White", precio:80}
+]
+const remeras = [
+    {id:1, nombre:"North Face", precio:20}
+]
+const zapatillas =[
+    {id:1, nombre:"Nike", precio:150}
+]
+for(const buzo of buzos)
+for(const remera of remeras)
+for(const zapatilla of zapatillas)
 
 while (seguirComprando === true) {
     if (producto === 1){
-        totalCompra = totalCompra + 250
+        carritos.push(buzo)
     } else if (producto === 2){
-        totalCompra = totalCompra + 60
+        carritos.push(remera)
     } else if (producto === 3){
-        totalCompra = totalCompra + 20
+        carritos.push(zapatilla)
     } else {
-        producto = parseInt(prompt("Selecciona un producto habilitado 1.Zapatilla - 2.Buzo - 3.Remera"))
+        producto = parseInt(prompt("Selecciona un producto habilitado 1.Buzo - 2.Remera - 3.Zapatillas"))
         continue
     }
 
    decision = parseInt (prompt("Quieres seguir comprando? 1.Si - 2.No"))
    if (decision === 1){
-    producto = parseInt(prompt("Selecciona el producto que deseas comprar 1.Zapatilla - 2.Buzo - 3.Remera"))
+    producto = parseInt(prompt("Selecciona el producto que deseas comprar 1.Buzo - 2.Remera - 3.Zapatillas"))
    } else {
     seguirComprando = false
    }
 }
 
-alert("El total de compra es " + totalCompra )
+let resultadoCompra = carritos.reduce((accum, p) => {
+    return accum + p.precio
+}, 0)
+
+alert(`El total de tu compra es ${resultadoCompra} dolares`)
+console.log(carritos);
